75b0ee09953beeb207cadcc6a86b263096be03d3
Özlem Albayrak <albayrak.ozlem@gmail.com>
2022-05-19 11:52:34 +0200

    Update Credentials.py

--- tweet_collector/Credentials.py
+++ tweet_collector/Credentials.py
@@ -1,4 +1,4 @@
-##API KEY: ANgYXBQtDTXMnhGnGYvWnrVuQ; 
-##API Key Secret: gTzbgG46nMYHWHPgXBLSVqMadOuGBBcG8gjDaRJOErGHxzQ8M0;
+##API KEY: *****; 
+##API Key Secret: *****;
 Bearer_Token='******'
-##Access Token:367024154-i74OOw9zrqK0rbkJFhwMW5ONv0LCP1xHYRunmU9T
+##Access Token:*******
